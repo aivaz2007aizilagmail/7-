b40a15338257b21cf012ec99527313c6ff96f2b1
Aivaz <105557584+aivaz2007aizilagmail@users.noreply.github.com>
2025-04-28 08:41:59 +0300

    Update poisk.c

--- poisk.c
+++ poisk.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
 
-// Функция для поиска слова в строке
 int indexOf(char text[], char word[]) {
     int textLength = 0;
     int wordLength = 0;
-
-    // Вычисляем длину строки text
     while (text[textLength] != '\0') {
         textLength++;
     }
-
-    // Вычисляем длину слова word
     while (word[wordLength] != '\0') {
         wordLength++;
     }
-
-    // Поиск слова в тексте
     for (int i = 0; i <= textLength - wordLength; i++) {
-        int match = 1; // Флаг совпадения
+        int match = 1; 
         for (int j = 0; j < wordLength; j++) {
             if (text[i + j] != word[j]) {
-                match = 0; // Несовпадение символов
+                match = 0; 
                 break;
             }
         }
         if (match == 1) {
-            return i; // Возвращаем индекс начала слова
+            return i; 
         }
     }
-
-    return -1; // Слово не найдено
+    return -1; //
 }
 
 int main() {
     char text[] = "the cat sat";
     char word[] = "cat";
-
     int position = indexOf(text, word);
-
     if (position != -1) {
         printf("Слово \"%s\" найдено на позиции %d\n", word, position);
     } else {
         printf("Слово \"%s\" не найдено\n", word);
     }
-
     return 0;
 }
